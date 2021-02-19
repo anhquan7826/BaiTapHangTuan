39bbfb532a98eff723c6221cc0cd654b31be5abb
anhquan7826 <anhquan7826@gmail.com>
2021-02-19 15:50:06 +0700

    BT4-B7

--- tuan3/source/B_7.cpp
+++ tuan3/source/B_7.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main() {
+    char input[2];
+    cin >> input;
+    for (int letter=0; letter<2; letter++) {
+        for (int i=0; i<2; i++) {
+            cout << input[letter] << input[i] << ' ';
+        }
+    }
+    for (int letter=0; letter<2; letter++) {
+        for (int i=0; i<2; i++) {
+            for (int j=0; j<2; j++) {
+                cout << input[letter] << input[i] << input[j] << ' ';
+            }
+        }
+    }
+    cout << endl;
     return 0;
 }
