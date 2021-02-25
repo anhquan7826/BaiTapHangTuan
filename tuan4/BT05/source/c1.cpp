da6cb27287f17539d0e0e1ef000a211c75cfd3ab
anhquan7826 <anhquan7826@gmail.com>
2021-02-25 19:00:30 +0700

    fixed segfault & random

--- tuan4/BT05/source/c1.cpp
+++ tuan4/BT05/source/c1.cpp
@@ -1,60 +1,14 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
-void turn_into_actual_minefield(char** field, int m, int n) {
-    for (int i=0; i<m; i++) {
-        for (int j=0; j<n; j++) {
-            int Count=0;
-            if (*(*(field+i)+j) != '*') {
-                for (int a=i-1; a<=i+1; a++) {
-                    if (a<0) {continue;}
-                    for (int b=j-1; b<=j+1; b++) {
-                        if (b<0) {continue;}
-                        if (*(*(field+a)+b) == '*') {
-                            Count++;
-                        }
-                    }
-                }
-                *(*(field+i)+j) = (char)(Count+48);
-            }
-        }
-    }
-}
-
-char** generate_minefield(int m, int n, int k) {
-    char **generatedField = new char* [m];
-    for (int i=0; i<m; i++) {
-        *(generatedField+i) = new char [n];
-    }
-    for (int i=0; i<m; i++) {
-        for (int j=0; j<n; j++) {
-            *(*(generatedField+i)+j) = '.';
-        }
-    }
-    while (k != 0) {
-        srand(k);
-        int a = rand()%m;
-        int b = rand()%n;
-    //  cout << a << ' ' << b << endl;
-        if (*(*(generatedField+a)+b) == '.') {
-            *(*(generatedField+a)+b) = '*';
-            k--;
-        }
-    }
-    return generatedField;
-}
-
-void print_minefield(char** field, int m, int n) {
-    for (int i=0; i<m; i++) {
-        for (int j=0; j<n; j++) {
-            cout << *(*(field+i)+j) << ' ';
-        }
-        cout << endl;
-    }
-}
+void generate_random_mine(vector<vector<char>> &field, int m, int n, int k);
+void setup_tiles(vector<vector<char>> &field, int m, int n);
+void print_field(vector<vector<char>> &field, int m, int n);
+void render_game();
 
 int main() {
     int m, n, k;
@@ -66,11 +20,62 @@ int main() {
         cout << "so bom vuot qua gioi han!" << endl;
         return 0;
     }
+    vector<vector<char>> field (n, vector<char> (m));
     cout << "generating minefield..." << endl;
-    char** mineField = generate_minefield(m, n, k);
-    print_minefield(mineField, m, n);
+    generate_random_mine(field, m, n, k);
+    cout << "done!" << endl;
+    print_field(field, m, n);
     cout << "converting minefield..." << endl;
-    turn_into_actual_minefield(mineField, m, n);
-    print_minefield(mineField, m, n);
+    setup_tiles(field, m, n);
+    cout << "done!" << endl;
+    print_field(field, m, n);
     return 0;
+}
+
+void generate_random_mine(vector<vector<char>> &field, int m, int n, int k) {
+    srand(time(0));
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            field[i][j] = '.';
+        }
+    }
+    while (k != 0) {
+        int a = rand()%m;
+        int b = rand()%n;
+        if (field[a][b] != '*') {
+            field[a][b] = '*';
+            cout << k << endl;
+            k--;
+        }
+    }
+}
+
+void print_field(vector<vector<char>> &field, int m, int n) {
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            cout << field[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+void setup_tiles(vector<vector<char>> &field, int m, int n) {
+    int Count;
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            Count=0;
+            if (field[i][j] != '*') {
+                for (int a=i-1; a<=i+1; a++) {
+                    if (a<0 || a>=m) {continue;}
+                    for (int b=j-1; b<=j+1; b++) {
+                        if (b<0 || b >= n) {continue;}
+                        if (field[a][b] == '*') {
+                            Count++;
+                        }
+                    }
+                }
+                field[i][j] = (char)(Count+48);
+            }
+        }
+    }
 }
